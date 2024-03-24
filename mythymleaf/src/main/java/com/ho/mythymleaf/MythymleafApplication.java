458ace149eea74928d760c74fe6a8b3704acbc49
SungHo Ha <skyg547@daiso.co.kr>
2024-03-24 21:26:33 +0900

    feat: reuse springBootapplication
    reuse all in one annotation
    
    issueX

--- mythymleaf/src/main/java/com/ho/mythymleaf/MythymleafApplication.java
+++ mythymleaf/src/main/java/com/ho/mythymleaf/MythymleafApplication.java
@@ -1,22 +1,13 @@
 package com.ho.mythymleaf;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.WebApplicationType;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//@SpringBootApplication
-@Configuration
-@ComponentScan
-@EnableAutoConfiguration
+@SpringBootApplication
 public class MythymleafApplication {
 
 	public static void main(String[] args) {
-		SpringApplication application = new SpringApplication(MythymleafApplication.class);
-		application.setWebApplicationType(WebApplicationType.NONE);
-		application.run(args);
-//		SpringApplication.run(MythymleafApplication.class, args);
+		SpringApplication.run(MythymleafApplication.class, args);
 	}
 
 }
