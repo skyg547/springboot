268932f7ec1cb017a23ad00230bac2b362812cfe
SungHo Ha <skyg547@daiso.co.kr>
2024-03-24 21:22:20 +0900

    feat: springbootapplication change enadble auto configuration componet scan configuration
    get detail configuration
    
    issueX

--- mythymleaf/src/main/java/com/ho/mythymleaf/MythymleafApplication.java
+++ mythymleaf/src/main/java/com/ho/mythymleaf/MythymleafApplication.java
@@ -1,13 +1,22 @@
 package com.ho.mythymleaf;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
+//@SpringBootApplication
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
 public class MythymleafApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MythymleafApplication.class, args);
+		SpringApplication application = new SpringApplication(MythymleafApplication.class);
+		application.setWebApplicationType(WebApplicationType.NONE);
+		application.run(args);
+//		SpringApplication.run(MythymleafApplication.class, args);
 	}
 
 }
